d7612236c051c61ed90d1457822babdf5b9a7ba6
Vinicius Botelho <viniciusmcode@gmail.com>
2025-04-25 14:31:57 -0300

    feat: added homepage base structure

---
+++ frontend/src/pages/home/Home.tsx
@@ -0,0 +1,16 @@
+import styles from "./home.module.css";
+import logo from "../../assets/Logo.svg";
+import AddTask from "../../components/AddTask/AddTask"
+
+const Home = () => {
+  return (
+    <>
+      <header className={styles.mainHeader}>
+        <img className={styles.logoImg} src={logo} alt="todo" />
+      </header>
+      <AddTask/>
+    </>
+  );
+};
+
+export default Home;
