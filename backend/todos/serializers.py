ccf0cfa320a72c3213ead1908005064f1f00926f
Gabriel <ramos.gabrielsilva23@gmail.com>
2025-04-25 08:05:47 -0300

    remove: untrack system files

--- backend/todos/serializers.py
+++ backend/todos/serializers.py
@@ -5,4 +5,4 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ('tittle', 'body', 'create_date')
+        fields = '__all__'
