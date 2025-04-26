8c99e9e0e7080836aa6547cad502aa01be5c4fbd
Vinicius Botelho <viniciusmcode@gmail.com>
2025-04-26 01:32:58 -0300

    fix: home components structure and styles fixed

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,16 +1,16 @@
 import styles from "./home.module.css";
 import logo from "../../assets/Logo.svg";
-import AddTask from "../../components/addTask/AddTask"
-import TaskBoard from "../../components/taskBoard/TaskBoard"
+import TaskBoard from "../../components/taskBoard/TaskBoard";
 
 const Home = () => {
   return (
     <>
-      <div className={styles.mainHeader}>
+      <header className={styles.mainHeader}>
         <img className={styles.logoImg} src={logo} alt="todo" />
-      <AddTask/>
-      </div>
-      <TaskBoard/>
+      </header>
+      <main className={styles.mainContainer}>
+        <TaskBoard />
+      </main>
     </>
   );
 };
