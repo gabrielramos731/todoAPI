eac6b94a59341419ddfcb45278c27307167b8a06
icaroivcd <icaro.v84@gmail.com>
2025-04-30 20:07:58 -0300

    feat: add url to create method

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListTodo , DetailTodo
+from .views import ListTodo , DetailTodo, CreateTodo
 
 urlpatterns = [ 
     path('<int:pk>', DetailTodo.as_view(), name = 'todo_detail'),
     path('', ListTodo.as_view(), name = 'todo_list'),
+    path('create/', CreateTodo.as_view(), name='todo_create')
 ]
