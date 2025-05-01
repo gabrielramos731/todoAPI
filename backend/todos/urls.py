6f0093b9eec34f6d00535814c966008b8e96a492
icaroivcd <icaro.v84@gmail.com>
2025-05-01 10:42:23 -0300

    feat: add update url to project

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ListTodo , DetailTodo, CreateTodo
+from .views import ListTodo , DetailTodo, CreateTodo, UpdateTodo
 
 urlpatterns = [ 
     path('<int:pk>', DetailTodo.as_view(), name = 'todo_detail'),
     path('', ListTodo.as_view(), name = 'todo_list'),
-    path('create/', CreateTodo.as_view(), name='todo_create')
+    path('create/', CreateTodo.as_view(), name='todo_create'),
+    path('update/<int:pk>', UpdateTodo.as_view(), name='todo_update'),
 ]
