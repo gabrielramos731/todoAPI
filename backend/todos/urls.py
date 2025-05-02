ca2b244a5f717f47cecdb6244794fc42c234bd6c
icaroivcd <icaro.v84@gmail.com>
2025-05-01 21:21:47 -0300

    feat: add url to delete method

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListTodo , DetailTodo, CreateTodo, UpdateTodo
+from .views import ListTodo , DetailTodo, CreateTodo, UpdateTodo, DeleteTodo
 
 urlpatterns = [ 
     path('<int:pk>', DetailTodo.as_view(), name = 'todo_detail'),
     path('', ListTodo.as_view(), name = 'todo_list'),
-    path('create/', CreateTodo.as_view(), name='todo_create'),
-    path('update/<int:pk>', UpdateTodo.as_view(), name='todo_update'),
+    path('create/', CreateTodo.as_view(), name ='todo_create'),
+    path('update/<int:pk>', UpdateTodo.as_view(), name = 'todo_update'),
+    path('delete/<int:pk>', DeleteTodo.as_view(), name = 'todo_delete'),
 ]
