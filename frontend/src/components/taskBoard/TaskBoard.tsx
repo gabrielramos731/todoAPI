d873e132285e99e7e1e8242adf4b28b2737c96d6
Vinicius Botelho <viniciusmcode@gmail.com>
2025-05-10 19:47:54 -0300

    feat: set an animation when task was set as done

--- frontend/src/components/taskBoard/TaskBoard.tsx
+++ frontend/src/components/taskBoard/TaskBoard.tsx
@@ -2,34 +2,48 @@ import styles from "./TaskBoard.module.css";
 import Task from "../task/Task";
 import AddTask from "../addTask/AddTask";
 import useTaskManager from "../../hooks/useTaskManager";
+import { AnimatePresence, motion } from "framer-motion";
 
 const TaskBoard = () => {
   const { tasks, addTask, deleteTask, toggleDone, sortTasks } = useTaskManager();
 
+
   return (
     <>
-    
       <AddTask onAddTask={addTask} />
       <div className={styles.taskBoardStatus}>
         <p className={styles.taskCreated}>
-          Tarefas criadas: <span className={styles.taskCount}>{tasks.length}</span>
+          Tarefas criadas:{" "}
+          <span className={styles.taskCount}>{tasks.length}</span>
         </p>
         <p className={styles.taskDone}>
-          Concluídas: <span className={styles.taskDoneCount}>{tasks.filter((task) => task.completed).length}</span>
+          Concluídas:{" "}
+          <span className={styles.taskDoneCount}>
+            {tasks.filter((task) => task.completed).length}
+          </span>
         </p>
       </div>
       <div className={styles.taskDiv}>
         {tasks.length === 0 ? (
           <p className={styles.noTaskMessage}>Nenhuma tarefa cadastrada.</p>
         ) : (
-          sortTasks().map((task) => (
-            <Task
-              key={task.id}
-              task={task}
-              onDelete={deleteTask}
-              toggleDone={toggleDone}
-            />
-          ))
+          <AnimatePresence>
+            {sortTasks().map((task) => (
+              <motion.div
+                key={task.id}
+                layout
+
+                
+                >
+                <Task
+                  key={task.id}
+                  task={task}
+                  onDelete={deleteTask}
+                  toggleDone={toggleDone}
+                />
+              </motion.div>
+            ))}
+          </AnimatePresence>
         )}
       </div>
     </>
