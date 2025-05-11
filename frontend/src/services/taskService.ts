f17fac3e5af3cb48a30f16120e56d1161b2c71a8
Vinicius Botelho <viniciusmcode@gmail.com>
2025-05-11 17:02:47 -0300

    feat: all http methods implemented

--- frontend/src/services/taskService.ts
+++ frontend/src/services/taskService.ts
@@ -1,18 +1,24 @@
 import axiosInstance from "../api/axiosInstance";
 import { Tasktype } from "../hooks/useTaskManager";
 
-export const getTasks = async ():Promise<Tasktype[]> => {
+export const getTasks = async (): Promise<Tasktype[]> => {
   const response = await axiosInstance.get("/");
   return response.data;
 };
 
-export const createTask = async (data: Omit<Tasktype, 'id'>):Promise<Tasktype> => {
+export const createTask = async (
+  data: Omit<Tasktype, "id">
+): Promise<Tasktype> => {
   const response = await axiosInstance.post("/create/", data);
   return response.data;
 };
 
-export const deleteTask = async(id: number):Promise<void> => {
+export const deleteTask = async (id: number): Promise<void> => {
   await axiosInstance.delete(`/delete/${id}`);
 };
 
-
+export const updateTask = async (data: Tasktype): Promise<Tasktype> => {
+  console.log(data)
+  const response = await axiosInstance.put(`/update/${data.id}`, data);
+  return response.data;
+};
